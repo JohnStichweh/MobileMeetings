6424eafeb06b1b48f1a82d179ebbecaecad4a73a
Ruairi Blake <Rblake0601@gmail.com>
2021-06-11 17:55:41 -0400

    inputs and storing data on frontend

---
+++ app/src/main/java/com/it3048/mobiledeviceproject/AddMeetingForm.kt
@@ -0,0 +1,54 @@
+package com.it3048.mobiledeviceproject
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.add_meeting_form.view.*
+
+class AddMeetingForm: DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       var rootView: View = inflater.inflate(R.layout.add_meeting_form, container, false)
+
+        rootView.cancelButton.setOnClickListener{
+            dismiss()
+            rootView.editTextMeetingTitle.setText("")
+            rootView.meetingColor.selectedItem.toString()
+            rootView.meetingDate.setText("")
+            rootView.editTextDescripton.setText("")
+            rootView.editTextLocLink.setText("")
+        }
+
+        rootView.submitButton.setOnClickListener{
+            //this is where the inputs get saved to state
+            var meetingTitle = rootView.editTextMeetingTitle.text.toString()
+            var meetingColor = rootView.meetingColor.selectedItem.toString()
+            var meetingDate = rootView.meetingDate.text.toString()
+            var meetingDescription = rootView.editTextDescripton.text.toString()
+            var meetingLocLink = rootView.editTextLocLink.text.toString()
+
+            Toast.makeText(context, "submitted", Toast.LENGTH_LONG).show()
+            Log.i(meetingTitle, "meetingTitle")
+            Log.i(meetingColor, "meetingColor")
+            Log.i(meetingDate, "meetingDate")
+            Log.i(meetingDescription, "meetingDescription")
+            Log.i(meetingLocLink, "meetingLocLink")
+
+
+            dismiss()
+
+        }
+
+
+
+        return rootView
+    }
+}
--- app/src/main/java/com/it3048/mobiledeviceproject/MainActivity.kt
+++ app/src/main/java/com/it3048/mobiledeviceproject/MainActivity.kt
@@ -2,23 +2,31 @@ package com.it3048.mobiledeviceproject
 
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.widget.*
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    var addMeetingForm = AddMeetingForm()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { addForm() }
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { addMeetingForm.show(supportFragmentManager, "addForm") }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,28 +45,71 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun addForm() {
-        val dialog: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
-        dialog.setMessage("Input meeting details")
-        dialog.setTitle("New Meeting")
-        //TODO: @RUAIRI insert inputs here
-        //title
-        //date/time
-        //description
-        //location/link
-        //color
-
-        dialog.setPositiveButton("Submit",
-            DialogInterface.OnClickListener { dialog, which ->
-                Toast.makeText(applicationContext, "Submitted", Toast.LENGTH_LONG)
-                    .show()
-            })
-        dialog.setNegativeButton("cancel",
-            DialogInterface.OnClickListener { dialog, which ->
-                Toast.makeText(applicationContext, "Cancelled", Toast.LENGTH_LONG)
-                    .show()
-            })
-        val alertDialog: android.app.AlertDialog? = dialog.create()
-        alertDialog?.show()
-    }
+//    private fun addForm() {
+//        val dialog: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this)
+//
+//        //TODO: @RUAIRI create a style or layout for this form
+////        val dialog: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(
+////            ContextThemeWrapper(
+////                this,
+////                R.style.addForm
+////            )
+////        )
+//
+//        val colors = resources.getStringArray(R.array.colors)
+//        //dialog.setMessage("Input meeting details")
+//        dialog.setTitle("New Meeting")
+//        val layout = LinearLayout(this)
+//        layout.orientation = LinearLayout.VERTICAL
+//
+//        //TODO: @RUAIRI insert inputs here
+//
+//        //title
+//        val meetingTitle = EditText(this)
+//        meetingTitle.setSingleLine()
+//        meetingTitle.hint = "Title"
+//        layout.addView(meetingTitle)
+//
+//        //date/time
+////        val today = Calendar.getInstance().time
+////        val datePicker = DatePicker(
+////            ContextThemeWrapper(
+////                this,
+////                R.style.datePicker
+////            )
+////        )
+////        //datePicker.minDate = today
+////        layout.addView(datePicker)
+//
+//        //description
+//        val meetingDesc = EditText(this)
+//        meetingDesc.hint = "Description"
+//        layout.addView(meetingDesc)
+//
+//        //location/link
+//        val meetingLoc = EditText(this)
+//        meetingLoc.hint = "Location or link"
+//        layout.addView(meetingLoc)
+//
+//        //color
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, colors)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        val spinner = Spinner(this)
+//        spinner.adapter = adapter
+//        layout.addView(spinner)
+//
+//        dialog.setPositiveButton("Submit",
+//            DialogInterface.OnClickListener { dialog, which ->
+//                Toast.makeText(applicationContext, "Submitted", Toast.LENGTH_LONG)
+//                    .show()
+//            })
+//        dialog.setNegativeButton("cancel",
+//            DialogInterface.OnClickListener { dialog, which ->
+//                Toast.makeText(applicationContext, "Cancelled", Toast.LENGTH_LONG)
+//                    .show()
+//            })
+//        val alertDialog: android.app.AlertDialog? = dialog.create()
+//        alertDialog?.setView(layout)
+//        alertDialog?.show()
+//    }
 }

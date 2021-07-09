d56842210543099db994862e4ad117b9f8b59d37
defiantturtle <sarabarnes078@gmail.com>
2021-07-09 09:43:30 -0400

    Basic design of meeting card

---
+++ app/src/main/java/com/it3048/mobiledeviceproject/MeetingAdapter.kt
@@ -0,0 +1,10 @@
+package com.it3048.mobiledeviceproject
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MeetingAdapter {
+    inner class meetingViewHolder(val v:View):RecyclerView.ViewHolder(v){
+        
+    }
+}
